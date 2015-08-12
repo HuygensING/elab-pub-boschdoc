8c10f86fcf83d80980af942e4c9fab42db8fdfca
Rene van der Ark <r.van.der.ark@gmail.com>
2015-08-12 15:50:00 +0200

    added static index.html

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -5,6 +5,7 @@ rm -rf build/development
 mkdir -p build/development/js
 mkdir build/development/css
 
+cp src/index.html build/development/index.html
 
 # Bundle React libs
 node_modules/.bin/browserify \
